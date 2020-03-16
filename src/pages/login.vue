1041587492cc9b4d55604d0ae39f17aa232d5205
zhenqiang-lu <13784569438@163.com>
2020-03-16 12:14:02 +0800

    first test

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <el-input placeholder="用户名" v-model="username"></el-input>
-    <el-input placeholder="密码" v-model="password"></el-input>
-    <el-button @click="login" type="primary">登陆</el-button>
+      <div class="login-box">
+          <span>WEB IPCAMERA</span>
+          <el-input class="msg-input" placeholder="用户名" v-model="username"></el-input>
+          <el-input class="msg-input" placeholder="密码" v-model="password"></el-input>
+          <el-button  class="msg-btn" @click="login" type="primary">登录</el-button>
+      </div>
+    
   </div>
 </template>
 
@@ -26,9 +30,12 @@
               username: this.username,
               password: this.password
           }
+          if(!sendData.username || !sendData.password){
+              this.$message.error("请输入用户名和密码！")
+              return;
+          }
           login(sendData).then(res => {
-              // console.log(res)
-              
+              this.$message.success(JSON.stringify(res))
           })
       }
     }
@@ -36,5 +43,20 @@
 </script>
 
 <style scoped>
-
+.login-box{
+    border-radius: 9px;
+    background: #EDEDED;
+    padding: 10px 30px;
+    text-align: center;
+    width: 400px;
+    margin: 10% auto 0;
+    box-shadow: 10px 10px 10px #EDEDED;
+}
+.login-box span{
+    font-size: 32px;
+    line-height: 60px;
+}
+.msg-input{
+    margin-bottom: 20px;
+}
 </style>
